3ada7e68505babaff8640c53f08523de6a2da0c1
abdessamadjalalinfo <53655103+abdessamadjalalinfo@users.noreply.github.com>
2024-04-08 15:02:51 +0000

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4829,7 +4829,7 @@ You can choose to not receive cookies from Google Analytics <a href="https://too
 </H3>
 <br>
 <div class="col-6">
-<form action="{{route('store')}}" method="post" enctype="multipart/form-data">
+<form action="{{route('store',[], true)}}" method="post" enctype="multipart/form-data">
 @csrf
   <div class="mb-3">
     <label for="exampleInputEmail1" class="form-label"><b> Email address</b></label>
