f93e8adaf3888fc1c0b47002d2e2279b09dab3bb
abdessamadjalalinfo <53655103+abdessamadjalalinfo@users.noreply.github.com>
2024-06-04 22:42:41 +0100

    Create jalal.js

---
+++ jalal.js
@@ -0,0 +1,82 @@
+<style>
+
+  body
+  {
+    zoom:70%;
+  }
+</style>
+
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script>
+        $(document).ready(function() {
+            // Vérifiez si l'URL contient "power-core-e90"
+            if (window.location.href.includes('power-core-e90')) {
+                // Masquer le corps de la page
+              //  $('body').css('display', 'none');
+$('.shopify-payment-button__button').css({
+    'font-size': '1.2em', // Changer la taille de la police
+    'font-weight': 'bold', // Mettre en gras
+    'text-transform': 'uppercase' // Forcer le texte en majuscules
+});
+         
+        $(document).ready(function() {
+            function blink() {
+                
+              $('.shopify-payment-button__button').fadeOut(100).fadeIn(700);
+              
+            }
+            setInterval(blink, 1000);
+        });
+
+              $(document).ready(function() {
+    var oldPrice = '(Clearance & Closeout - Sale)'; // L'ancien prix
+    var newPriceElement = $('.product__price');
+
+    // Ajouter l'ancien prix barré avant le nouveau prix
+    newPriceElement.after('<span class="old-price" style="text-decoration: ; margin-right: 10px;"><b>' + oldPrice + '</b></span>');
+});
+
+                           $('div[data-product-images][data-zoom="true"][data-has-slideshow="true"]').html('<img src="https://i.ibb.co/VgFgMZw/61-Q-lq-N6l1-L-AC-SX679.jpg" alt="New Product Image"><br><img src="https://i.ibb.co/3rW53Ym/71-V5-11k-FUL-AC-SL1500.jpg" alt="71-V5-11k-FUL-AC-SL1500" border="0">');
+
+                           $('.rte').eq(2).html(
+
+
+                             `
+                <table id="productDetails_techSpec_section_1" class="a-keyvalue prodDetTable" role="presentation">
+                 <iframe width="420" height="345" src="https://www.youtube.com/embed/j44Pb_Cp1k8?autoplay=1" allow='autoplay'>
+</iframe>
+                    <tbody>
+                        <tr><th class="a-color-secondary a-size-base prodDetSectionEntry">Brand</th><td class="a-size-base prodDetAttrValue">‎Arwibon</td></tr>
+                        <tr><th class="a-color-secondary a-size-base prodDetSectionEntry">Color</th><td class="a-size-base prodDetAttrValue">‎Black</td></tr>
+                        <tr><th class="a-color-secondary a-size-base prodDetSectionEntry">Age Range (Description)</th><td class="a-size-base prodDetAttrValue">‎Adult</td></tr>
+                        <tr><th class="a-color-secondary a-size-base prodDetSectionEntry">Special Feature</th><td class="a-size-base prodDetAttrValue">‎Dual Braking System,Foldable</td></tr>
+                        <tr><th class="a-color-secondary a-size-base prodDetSectionEntry">Weight Limit</th><td class="a-size-base prodDetAttrValue">‎264 Pounds</td></tr>
+                        <tr><th class="a-color-secondary a-size-base prodDetSectionEntry">Product Dimensions</th><td class="a-size-base prodDetAttrValue">‎50"L x 10.6"W x 20"H</td></tr>
+                        <tr><th class="a-color-secondary a-size-base prodDetSectionEntry">Number of Wheels</th><td class="a-size-base prodDetAttrValue">‎2</td></tr>
+                        <tr><th class="a-color-secondary a-size-base prodDetSectionEntry">Model Name</th><td class="a-size-base prodDetAttrValue">‎POWER CORE E90</td></tr>
+                        <tr><th class="a-color-secondary a-size-base prodDetSectionEntry">Wheel Material</th><td class="a-size-base prodDetAttrValue">‎Rubber</td></tr>
+                        <tr><th class="a-color-secondary a-size-base prodDetSectionEntry">Handlebar Type</th><td class="a-size-base prodDetAttrValue">‎Adjustable</td></tr>
+                        <tr><th class="a-color-secondary a-size-base prodDetSectionEntry">Wheel Size</th><td class="a-size-base prodDetAttrValue">‎11 Inches</td></tr>
+                        <tr><th class="a-color-secondary a-size-base prodDetSectionEntry">Wheel Type</th><td class="a-size-base prodDetAttrValue">‎vacuum</td></tr>
+                        <tr><th class="a-color-secondary a-size-base prodDetSectionEntry">Suspension Type</th><td class="a-size-base prodDetAttrValue">‎Dual Suspension</td></tr>
+                        <tr><th class="a-color-secondary a-size-base prodDetSectionEntry">Brake Style</th><td class="a-size-base prodDetAttrValue">‎Dual disc brake</td></tr>
+                        <tr><th class="a-color-secondary a-size-base prodDetSectionEntry">Warranty Type</th><td class="a-size-base prodDetAttrValue">‎Limited</td></tr>
+                        <tr><th class="a-color-secondary a-size-base prodDetSectionEntry">Recommended Uses For Product</th><td class="a-size-base prodDetAttrValue">‎City commuting, Off-road adventures</td></tr>
+                        <tr><th class="a-color-secondary a-size-base prodDetSectionEntry">Maximum Distance Range</th><td class="a-size-base prodDetAttrValue">‎40 Miles</td></tr>
+                        <tr><th class="a-color-secondary a-size-base prodDetSectionEntry">Manufacturer</th><td class="a-size-base prodDetAttrValue">‎Dongguan Lvdaoxing Technology Co., Ltd.</td></tr>
+                        <tr><th class="a-color-secondary a-size-base prodDetSectionEntry">Item Dimensions LxWxH</th><td class="a-size-base prodDetAttrValue">‎50 x 10.6 x 20 inches</td></tr>
+                        <tr><th class="a-color-secondary a-size-base prodDetSectionEntry">Brand Name</th><td class="a-size-base prodDetAttrValue">‎Arwibon</td></tr>
+                        <tr><th class="a-color-secondary a-size-base prodDetSectionEntry">Warranty Description</th><td class="a-size-base prodDetAttrValue">‎1 Year</td></tr>
+                        <tr><th class="a-color-secondary a-size-base prodDetSectionEntry">Material</th><td class="a-size-base prodDetAttrValue">‎Aluminum</td></tr>
+                        <tr><th class="a-color-secondary a-size-base prodDetSectionEntry">Suggested Users</th><td class="a-size-base prodDetAttrValue">‎unisex-adult</td></tr>
+                        <tr><th class="a-color-secondary a-size-base prodDetSectionEntry">Included Components</th><td class="a-size-base prodDetAttrValue">‎Rear Fender, Brake pads, Manual, Charger, Tool kit</td></tr>
+                    </tbody>
+                </table>
+             
+
+            `
+                                                );
+
+            }
+        });
+    </script>
